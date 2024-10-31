fb1e006aeeafa980f9e8e21393b9799bad78b660
Vitalii Spartesnyi <ukrwily@gmail.com>
2024-10-31 21:26:14 +0200

    PC Added setInputValid

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -5,21 +5,40 @@ import "./TaskInput.css";
 
 const TaskInput = (props) => {
   const [inputText, setinputText] = useState("");
+  const [isInputValid, setIsInputValid] = useState(true);
 
   const taskInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsInputValid(true);
+    }
     setinputText(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    props.onAddTask(inputText);
+    if (inputText.trim()) {
+      if (inputText.trim().length > 30) {
+        props.onAddTask(inputText.slice(0, 30) + "...");
+        setinputText("");
+      } else {
+        props.onAddTask(inputText);
+        setinputText("");
+      }
+    } else {
+      setIsInputValid(false);
+    }
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Tasks</label>
-        <input type="text" onChange={taskInputChangeHandler} />
+        <label style={{ color: isInputValid ? "black" : "red" }}>Tasks</label>
+        <input
+          type="text"
+          onChange={taskInputChangeHandler}
+          value={inputText}
+          style={{ border: isInputValid ? "1px solid black" : "2px solid red" }}
+        />
       </div>
       <Button type="submit">Add task</Button>
     </form>
